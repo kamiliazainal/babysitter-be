1fe321f83ef305021a488185afb891c4c80a79c8
kamiliazainal_cth <kamilia@cheetah.com.my>
2023-06-08 17:21:08 +0800

    remove unused code

--- database/migrations/2023_06_05_133202_create_children_details_table.php
+++ database/migrations/2023_06_05_133202_create_children_details_table.php
@@ -16,14 +16,12 @@ class CreateChildrenDetailsTable extends Migration
         Schema::create('children_details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('booking_id');
-            // $table->unsignedBigInteger('parent_id');
             $table->string('name');
             $table->integer('age');
             $table->string('gender');
             $table->timestamps();
 
             $table->foreign('booking_id')->references('id')->on('booking_details')->onDelete('cascade');
-            // $table->foreign('parent_id')->references('id')->on('parent_details')->onDelete('cascade');
         });
     }
 
